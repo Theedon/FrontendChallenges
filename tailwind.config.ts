78df5fcdb8e4c97242c4a7ae8eeba815f6f28857
Theedon <faniyantoyin@gmail.com>
2023-11-29 15:52:45 +0100

    chore(design): add style guide to tailwind config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,13 +7,24 @@ const config: Config = {
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-    extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
-      },
+    extend: {},
+    screens: {
+      sm: "375px", //Mobile
+      lg: "1440px", //Desktop
     },
+    colors: {
+      white: "hsl(0, 0%, 100%)",
+      "light-gray": "hsl(212, 45%, 89%)",
+      "grayish-blue": "hsl(220, 15%, 55%)",
+      "dark-blue": "hsl(218, 44%, 22%)",
+    },
+    fontSize: {
+      "15": "15px",
+    },
+    fontFamily: {
+      outfit: ["Outfit", "sans-serif"],
+    },
+    fontWeight: { "400": "400", "700": "700" },
   },
   plugins: [],
 };
