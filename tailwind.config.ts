41f6c10c065ca273d50eb959d0911478de7d32aa
Theedon <faniyantoyin@gmail.com>
2023-11-30 17:32:12 +0100

    feat(design): implement style guide for newsletter

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,20 +7,31 @@ const config: Config = {
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        outfit: ["Outfit", "sans-serif"],
+        roboto: ["Roboto", "sans-serif"],
+      },
+    },
     screens: {
       sm: "375px", //Mobile
       lg: "1440px", //Desktop
     },
     colors: {
-      white: "hsl(0, 0%, 100%)",
-      "light-gray": "hsl(212, 45%, 89%)",
-      "grayish-blue": "hsl(220, 15%, 55%)",
-      "dark-blue": "hsl(218, 44%, 22%)",
-    },
+      qrColors: {
+        white: "hsl(0, 0%, 100%)",
+        "light-gray": "hsl(212, 45%, 89%)",
+        "grayish-blue": "hsl(220, 15%, 55%)",
+        "dark-blue": "hsl(218, 44%, 22%)",
+      },
 
-    fontFamily: {
-      outfit: ["Outfit", "sans-serif"],
+      newsletterColors: {
+        tomato: "Hepta_Slab(4, 100%, 67%)",
+        grey: "hsl(231, 7%, 60%)",
+        "dark-slate-grey": "hsl(234, 29%, 20%)",
+        "charcoal-grey": "hsl(235, 18%, 26%)",
+        white: "hsl(0, 0%, 100%)",
+      },
     },
   },
   plugins: [],
