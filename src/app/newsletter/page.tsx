29a4d9568f45ecc06eccea26c9105125df7d45fa
Theedon <faniyantoyin@gmail.com>
2023-12-01 15:31:42 +0100

    feat: add basic functionality and form handling

--- src/app/newsletter/page.tsx
+++ src/app/newsletter/page.tsx
@@ -1,55 +1,100 @@
+"use client";
 import listIcon from "@/assets/newsletter/icon-list.svg";
 import successIcon from "@/assets/newsletter/icon-success.svg";
 import desktopImage from "@/assets/newsletter/illustration-sign-up-desktop.svg";
-import mobileImage from "@/assets/newsletter/illustration-sign-up-desktop.svg";
+import mobileImage from "@/assets/newsletter/illustration-sign-up-mobile.svg";
 import Image from "next/image";
+import { emailSchema, subscribeSchema } from "@/lib/validators/newsletter";
+import { useFormik } from "formik";
+import { useState } from "react";
+import { toFormikValidationSchema } from "zod-formik-adapter";
 
 function Newsletter() {
+  const [isLoading, setIsLoading] = useState<boolean>(false);
+  type FormValues = {
+    email: string;
+  };
+  const formik = useFormik<FormValues>({
+    initialValues: { email: "" },
+    onSubmit: async (values) => {
+      setIsLoading(true);
+      await new Promise((resolve) => setTimeout(resolve, 3000));
+      console.log(values.email);
+      setIsLoading(false);
+    },
+
+    validationSchema: toFormikValidationSchema(subscribeSchema),
+  });
+  const setButtonInvalid: () => boolean = () => {
+    const { errors, values } = formik;
+    if (errors.email || !values.email) {
+      return true;
+    }
+    return false;
+  };
   return (
-    <main className="font-roboto flex max-h-[70vh] max-w-[100vh] rounded-2xl">
-      <section className="flex flex-1 flex-col justify-center gap-5 px-5">
-        <h2 className="font-roboto text-2xl font-bold">Stay Updated!</h2>
+    <main className="flex max-h-[70vh] max-w-[100vh] rounded-2xl font-roboto">
+      <section className="flex min-w-[60%] flex-1 flex-col justify-center gap-5 px-5">
+        <h1 className="font-roboto text-3xl font-bold ">Stay Updated!</h1>
         <p className="text-sm">
           Join 60,000+ product managers receiving monthly updates on:
         </p>
-        <ul className="flex items-center text-sm ">
-          <Image alt="list icon" src={listIcon} className="mr-2 h-6 w-6" />
-          Product discovery and building what matters
-        </ul>
-        <ul className="flex items-center text-sm ">
-          <Image
-            alt="list icon"
-            src={listIcon}
-            className="items mr-2 h-6 w-6"
-          />
-          Measuring to ensure updates are a success
-        </ul>
-        <ul className="flex items-center text-sm ">
-          <Image alt="list icon" src={listIcon} className="mr-2 h-6 w-6" />
-          And much more!
+        <ul className="flex flex-col gap-2">
+          <li className="flex text-sm">
+            <Image alt="list icon" src={listIcon} className="mr-2 h-6 w-6" />
+            Product discovery and building what matters
+          </li>
+          <li className="flex text-sm ">
+            <Image
+              alt="list icon"
+              src={listIcon}
+              className="items mr-2 h-6 w-6"
+            />
+            Measuring to ensure updates are a success
+          </li>
+          <li className="flex text-sm ">
+            <Image alt="list icon" src={listIcon} className="mr-2 h-6 w-6" />
+            And much more!
+          </li>
         </ul>
 
-        <div className="">
-          <label htmlFor="email" className="text-xs font-bold">
-            Email address
-          </label>
+        <form className="flex flex-col gap-1" onSubmit={formik.handleSubmit}>
+          <div className="flex items-center justify-between text-xs font-bold">
+            <label htmlFor="email">Email address</label>
+            <p className=" text-newsletterColors-tomato">
+              {formik.touched.email && formik.errors.email}
+            </p>
+          </div>
+
           <input
+            {...formik.getFieldProps("email")}
             type="email"
             name="email"
             id="email"
-            className="border-newsletterColors-grey focus:border-newsletterColors-tomato focus:bg-qrColors-light-gray hover:border-newsletterColors-tomato h-10 w-full rounded-lg border-2 p-4 text-sm outline-none"
+            className={`h-12 w-full transform rounded-lg border border-newsletterColors-grey p-2 text-sm outline-none duration-300 hover:scale-x-105  focus:bg-qrColors-light-gray ${
+              formik.touched.email && formik.errors.email
+                ? "border-newsletterColors-tomato bg-[#FFE8E6] placeholder:text-newsletterColors-dark-slate-grey"
+                : undefined
+            }`}
             placeholder="email@company.com"
           />
-        </div>
-        <button className="text-newsletterColors-white bg-newsletterColors-dark-slate-grey h-10 w-full rounded-lg text-sm">
-          Subscribe to monthly newsletter
-        </button>
+
+          <button
+            type="submit"
+            disabled={setButtonInvalid()}
+            className={`mt-2 h-12 w-full scale-100 transform cursor-pointer rounded-lg bg-newsletterColors-dark-slate-grey text-sm font-bold text-newsletterColors-white duration-300 focus:scale-105 ${
+              setButtonInvalid() ? "opacity-80" : "undefined"
+            }`}
+          >
+            Subscribe to monthly newsletter
+          </button>
+        </form>
       </section>
       <section className="flex-1">
         <Image
           src={desktopImage}
           alt="image showing sign in"
-          className="w-fill h-fill lg:flex"
+          className="w-fill min-h-[80vh] lg:flex"
         />
       </section>
     </main>
