decd366accc4b0b2ffeaa0a2c555a41f806c8c37
Theedon <faniyantoyin@gmail.com>
2023-12-06 22:13:02 +0100

    feat(design): improve UI for nav and footer

--- src/components/blanding/Footer.tsx
+++ src/components/blanding/Footer.tsx
@@ -2,19 +2,28 @@ import bookmark_logo from "@/assets/blanding/logo-bookmark-black.svg";
 import facebook_logo from "@/assets/blanding/icon-facebook.svg";
 import twitter_logo from "@/assets/blanding/icon-twitter.svg";
 import Image from "next/image";
+
 function Footer() {
   return (
-    <footer className="fixed bottom-0 left-0 flex h-16 w-full items-center justify-between bg-blandingColors-dBlue px-10">
-      <section className="text-blandingColors-white flex items-center gap-4 text-sm">
+    <footer className="bottom-0 mb-0 flex h-[40vh] w-full flex-col items-center justify-center gap-5 overflow-y-hidden bg-blandingColors-dBlue px-10 md:h-16 md:flex-row md:justify-between">
+      <section className="flex flex-col items-center gap-4 text-sm text-blandingColors-white md:flex-row">
         <Image src={bookmark_logo} alt="bookmark logo" />
         <h3>FEATURES</h3>
         <h3>PRICING</h3>
         <h3>CONTACT</h3>
       </section>
 
-      <section className="flex gap-4">
-        <Image src={facebook_logo} alt="facebook logo" />
-        <Image src={twitter_logo} alt="twitter logo" />
+      <section className="flex gap-7">
+        <Image
+          className="scale-110 transform cursor-pointer duration-200 hover:scale-105"
+          src={facebook_logo}
+          alt="facebook logo"
+        />
+        <Image
+          className="scale-110 transform cursor-pointer duration-200 hover:scale-105"
+          src={twitter_logo}
+          alt="twitter logo"
+        />
       </section>
     </footer>
   );
--- src/components/blanding/Navbar.tsx
+++ src/components/blanding/Navbar.tsx
@@ -1,18 +1,53 @@
+"use client";
 import bookmark_logo from "@/assets/blanding/logo-bookmark.svg";
 import Image from "next/image";
 import Button from "./Button";
-
+import { useState } from "react";
+import { Menu } from "lucide-react";
+import facebook_logo from "@/assets/blanding/icon-facebook.svg";
+import twitter_logo from "@/assets/blanding/icon-twitter.svg";
 function Navbar() {
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setMenuOpen((s) => !s);
+  };
   return (
-    <nav className=" flex md:justify-between">
-      <div className="min-w-10 flex-1 ">
-        <Image src={bookmark_logo} alt="bookmark logo"></Image>
+    <nav className="fixed top-0 z-10 flex w-full bg-opacity-20 p-5 backdrop-blur-md md:justify-between">
+      <div className="min-w-10 flex-1">
+        <Image src={bookmark_logo} alt="bookmark logo" />
       </div>
 
-      <section className="flex-grow-1 flex flex-1 flex-shrink items-center justify-evenly font-rubik">
-        <h3>FEATURES</h3>
-        <h3>PRICING</h3>
-        <h3>CONTACT</h3>
+      <div>
+        <Menu className="z-50 cursor-pointer md:hidden" onClick={toggleMenu} />
+        <section
+          className={`${
+            menuOpen ? "flex " : "hidden "
+          } fixed inset-0 z-[-1] h-[100vh] w-[100vw] transform flex-col justify-center gap-5 bg-blandingColors-sBlue opacity-90 backdrop-blur-md transition-transform duration-300 ease-in-out md:hidden`}
+        >
+          <Button variant="navigation">FEATURES</Button>
+          <Button variant="navigation">PRICING</Button>
+          <Button variant="navigation">CONTACT</Button>
+
+          <section className="flex items-center justify-center gap-5">
+            <Image
+              className="scale-110 transform cursor-pointer duration-200 hover:scale-105"
+              src={facebook_logo}
+              alt="facebook logo"
+            />
+            <Image
+              className="scale-110 transform cursor-pointer duration-200 hover:scale-105"
+              src={twitter_logo}
+              alt="twitter logo"
+            />
+          </section>
+        </section>
+      </div>
+
+      <section className="flex-grow-1 hidden flex-1 flex-shrink items-center justify-evenly font-rubik md:flex">
+        <Button variant="navigation">FEATURES</Button>
+        <Button variant="navigation">PRICING</Button>
+        <Button variant="navigation">CONTACT</Button>
         <Button size="medium">LOGIN</Button>
       </section>
     </nav>
